8c6d62a93ee221b6e50f0fd4908c7940270709af
VasylMalion <61274944+VasylMalion@users.noreply.github.com>
2025-06-16 15:13:36 +0000

    feat: add github actions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  res.send('Server is working v3.0!')
+  res.send('Server is working v4.0!')
 })
 
 app.listen(port, () => {
