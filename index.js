771b565031fc817d9c55fa0910205e8704ec2298
VasylMalion <61274944+VasylMalion@users.noreply.github.com>
2025-06-16 17:28:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  res.send('Server is working!')
+  res.send('Server is working v2.0!')
 })
 
 app.listen(port, () => {
